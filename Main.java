da9c42426209af9f2128758f4eaec19ad6443a4d
YoshEurrieBernas <Yosheurriebernas.scc@gmail.com>
2025-09-08 04:12:14 +0000

    Seatwork

--- Main.java
+++ Main.java
@@ -1,4 +1,16 @@
-public class Main{
+public class Main {
+  public static void main(String[] args) {
+    int countdown = 3;
+    System.out.println("Seatworkfive");
+
+    while (countdown > 0) {
+      System.out.println(countdown);
+      countdown--;
+    }
+    
+    System.out.println("Happy Birthday!!");
+  }
+} 
    public static void main(String[] args) {
     System.out.println("Hello Java!");
    } 
